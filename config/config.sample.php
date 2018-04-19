2c4a1248324dc6ee93bd273c93b6c72c2ca20535
Yeshwanth Vuppu <vyeshwanth123@gmail.com>
2018-04-20 02:24:06 +0530

    Add team logos, configuration

---
+++ config/config.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * set server error reporting
+ */
+ini_set('display_errors', 'on');
+ini_set('error_reporting', 'on');
+
+/**
+ * configuration
+ */
+$config = array();
+$config['host'] = 'localhost';
+$config['db'] = 'dbs_project';
+$config['username'] = 'root';
+$config['password'] = '';
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
-    echo 'Welcome';
-?>
+
+require_once('./config/config.php');
+require_once ('./lib/Database.php');
+
+$db = new Database($config);
+$connection_status = $db->connect();
+
+if(!$connection_status)
+{
+    die('Connection to database failed');
+}
+
